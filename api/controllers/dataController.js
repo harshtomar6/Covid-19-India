dbb55102871b3f1496bb3bc6aa9095c3c8f6718d
Harsh Tomar <harshtomar6@gmail.com>
2020-04-16 18:24:20 +0530

    Fixed Scraper

--- api/controllers/dataController.js
+++ api/controllers/dataController.js
@@ -27,7 +27,40 @@ const fetchLatestData = async () => {
   }
 }
 
+// Get Corono rising report
+async function getCoronaTrend() {
+  try {
+    let data = await CoronaData.find();
+    let tempObj = {}, obj = {};
+
+    for(let dataPoint of data) {
+      let date = new Date(dataPoint.createdAt).toDateString();
+      if(!tempObj.hasOwnProperty(date))
+        tempObj[date] = []
+      
+      tempObj[date].push(dataPoint)
+    }
+
+    for(let dataPoint of data) {
+      let date = new Date(dataPoint.createdAt).toDateString();
+      let currentData = tempObj[date];
+
+      let highest = currentData[0];
+      for(let currentd of currentData) {
+        if(parseInt(currentd.totalActiveCases) > parseInt(highest.totalActiveCases))
+          highest = currentd;
+      }
+      obj[date] = highest;
+    }
+
+    return { err: null, status: 200, data: obj };
+  } catch (err) {
+    return { err: err.toString(), status: 500, data: null }
+  }
+}
+
 module.exports = {
   getCoronaData,
-  fetchLatestData
+  fetchLatestData,
+  getCoronaTrend
 }
--- api/routes/scrapRouter.js
+++ api/routes/scrapRouter.js
@@ -1,11 +1,15 @@
 // Dependencies
 const express = require('express')
 const router = express.Router();
-const { fetchLatestData } = require('./../controllers/dataController');
+const { fetchLatestData, getCoronaTrend } = require('./../controllers/dataController');
 
 router.get('/latest', async (req, res) => {
   let { err ,status , data } = await fetchLatestData()
   res.status(status).send({ err, data })
+});
+
+router.get('/trend', async (req, res) => {
+  let data = await getCoronaTrend();
 })
 
 module.exports = router
